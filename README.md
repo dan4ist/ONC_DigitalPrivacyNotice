d63568c566abdba3eb3e2b4f1bcd52dc4c0071ad
dan4ist <dan4ist@gmail.com>
2014-04-07 12:26:42 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+/*******************************
+Digital Privacy Notice README.md
+*******************************/
+This website is using a parallax effect to achieve different scroll speeds of elements against their backgrounds.  It does this using a couple JavaScript libraries, HTML, and CSS.  There are very few actual files in this solution and images can be easily swapped by simple editing of the style.css file along with the other html files that contain all the privacy practice text.  Enjoy!
+
+-Daniel Forrest
+
+/*******************************/
+
+These page uses the following jQuery plugins:
+
+-Stellar.js 
+	http://markdalgleish.com/projects/stellar.js/
+-Waypoints
+	http://imakewebthings.com/jquery-waypoints/
+-jQuery Easing Plugin
+	http://gsgd.co.uk/sandbox/jquery/easing/
+
+CSS:
+-Google Fonts
+	https://www.google.com/fonts
+
+-Images taken from: http://www.freeimages.com/
+
+
+/*******************************
+INSTRUCTIONS
+*******************************/
+
+Providers should customize the following files:
+	-HCP File: NPP_HC.html (HCP)
+	-Health Plan File: NPP_HealthPlan.html (Health Plan)
+	-css/style.css
+
+Some notes on editing text:
+
+-To add another list item, enclose your text in <li>text content</li>
+-To add regular text, enclose text in <p></p>
+-links can be added using <a href="http://websiteyouwantToLinkto.com">Text</a>
+-CTRL-F/command+F to find Effective Date & replace the text.
+
+Logo:
+-Find the logo section of each html page and input any text you'd like.  The image can be edited in style.css
+	-Open style.css from within the css folder and find ".logo"
+		-From there, you will be able to add your own specific image.  Please place your image in the images folder prior to 
+
+If errors occur with implementation, please contact: danny@dannyforrest.com
---
+++ js/main.js
@@ -0,0 +1,93 @@
+jQuery(document).ready(function ($) {
+
+	//initialize Stellar.js
+	$(window).stellar();
+	
+	//cache some variables
+	var lastScrollTop = 0;
+	var links = $('.navigation').find('li');
+	slide = $('.transition');
+	button = $('.button');
+	mywindow = $(window);
+	htmlbody = $('html,body');
+	mover = $('.mover');
+	var sticky = $('#sticky');
+
+	//Setup waypoints plugin
+	slide.waypoint(function (event, direction) {
+		//cache the variable of the data-slide attribute associated with each slide
+		dataslide = $(this).attr('data-slide');
+
+		//If the user scrolls up, change the navigation link that has the same data-slide attribute as the slide to active 
+		//and remove the active class from the previous navigation link
+		if (direction == 'down') {
+			$('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+		} else {
+			$('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+		}
+	});
+
+	//waypoints doesnt detect the first slide when a user scrolls back up to the top
+	//so we add the below that removes the class from navigation link slide 2 and adds it to navigation link slide 1
+	mywindow.scroll(function() {
+		if(mywindow.scrollTop() == 0) {
+			$('.navigation li[data-slide="1"]').addClass('active');
+			$('.navigation li[data-slide="2"]').removeClass('active');	
+		}
+
+		//handle left scrolling of section titles
+		var bar = $('.mover');
+		var w = bar.width();
+		var st = $(this).scrollTop();
+		var left = bar.position().left;
+		if (st < lastScrollTop) {
+			//bar.css("width", w-2);
+			bar.css("left", left-.75);
+		} else {
+			//bar.css("width", w+2);
+			bar.css("left", left+.75);
+		}
+
+		lastScrollTop = st;
+	});
+
+	//A function that will be passed a slide number and then will scroll to that slide using jquerys animate.  
+	//We also passed in the easing method of 'easeInOutQuint' which is available via the easing plugin
+	function goToByScroll(dataslide) {
+		htmlbody.animate({
+			scrollTop: $('.transition[data-slide="' + dataslide + '"]').offset().top
+		}, 2000, 'easeInOutQuint');
+	}
+
+	//When a user clicks on the navigation links, get the data-slide attribute value
+	//and pass to the goToByScroll function
+	links.click(function (e) {
+		e.preventDefault();
+		dataslide = $(this).attr('data-slide');
+		goToByScroll(dataslide);
+	});
+
+	$('.oneline').hover(function() {
+    	$(this).animate({
+        	height: $(this)[0].scrollHeight+'px',
+        	backgroundColor: 'rgba(0, 0, 0, 0.9)'
+    	}, 300);
+	},function() {
+	    $(this).animate({
+	        height: '30px',
+	        backgroundColor: 'rgba(0, 0, 0, 0.75)'
+    	}, 300);
+	});
+	
+	$('.twoline').hover(function() {
+    	$(this).animate({
+        	height: $(this)[0].scrollHeight+'px',
+        	backgroundColor: 'rgba(0, 0, 0, 0.9)'
+    	}, 300);
+	},function() {
+	    $(this).animate({
+	        height: '60px',
+	        backgroundColor: 'rgba(0, 0, 0, 0.75)'
+    	}, 300);
+	});
+});
